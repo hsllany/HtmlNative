7a11529062dc037aea9fabb437cc4a8498dbcb37
hsllany <hsllany@163.com>
2017-05-19 15:45:08 +0800

    complete the float layout

--- HtmlNativeAndroid/htmlnative-lib/src/main/java/com/mozz/htmlnative/view/HNDiv.java
+++ HtmlNativeAndroid/htmlnative-lib/src/main/java/com/mozz/htmlnative/view/HNDiv.java
@@ -4,14 +4,19 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.IntDef;
 import android.util.ArrayMap;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mozz.htmlnative.css.Background;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -24,10 +29,10 @@ public class HNDiv extends ViewGroup implements IBackgroundView {
     private static final String TAG = HNDiv.class.getSimpleName();
 
     private List<List<View>> mAllViews = new ArrayList<>();
+    private List<View> mFloatLeftViews = new LinkedList<>();
+    private List<View> mFloatRightViews = new LinkedList<>();
     private List<Integer> mLineLength = new ArrayList<>();
-
     private BackgroundManager mBackgroundMgr;
-
     private Map<String, Object> mSavedInheritStyles = new ArrayMap<>();
 
     public HNDiv(Context context) {
@@ -55,50 +60,55 @@ public class HNDiv extends ViewGroup implements IBackgroundView {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
+        mFloatLeftViews.clear();
+        mFloatRightViews.clear();
+
         final int msWidth = MeasureSpec.getSize(widthMeasureSpec);
         final int msHeight = MeasureSpec.getSize(heightMeasureSpec);
-
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-
         int width = 0;
         int height = getPaddingLeft();
-
         int length = getChildCount();
-
         int lineWidth = getPaddingLeft();
         int lineHeight = 0;
-
         boolean firstLine = true;
-
         for (int i = 0; i < length; i++) {
             View child = getChildAt(i);
 
             measureChild(child, widthMeasureSpec, heightMeasureSpec);
 
-            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            HNDivLayoutParams lp = (HNDivLayoutParams) child.getLayoutParams();
 
-            int childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
-            int childHeight = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+            if (lp.positionMode == HNDivLayoutParams.POSITION_STATIC || lp.positionMode ==
+                    HNDivLayoutParams.POSITION_RELATIVE) {
 
-            if (childWidth + lineWidth + getPaddingRight() > msWidth) {
-                width = Math.max(lineWidth + getPaddingRight(), childWidth + getPaddingLeft());
-                height += lineHeight;
+                int childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+                int childHeight = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
 
-                lineWidth = childWidth + getPaddingLeft();
-                lineHeight = childHeight;
+                if (childWidth + lineWidth + getPaddingRight() > msWidth) {
+                    width = Math.max(lineWidth + getPaddingRight(), childWidth + getPaddingLeft());
+                    height += lineHeight;
 
-                if (firstLine) {
-                    firstLine = false;
+                    lineWidth = childWidth + getPaddingLeft();
+                    lineHeight = childHeight;
+
+                    if (firstLine) {
+                        firstLine = false;
+                    }
+                } else {
+                    lineWidth += childWidth;
+                    lineHeight = Math.max(lineHeight, childHeight);
                 }
-            } else {
-                lineWidth += childWidth;
-                lineHeight = Math.max(lineHeight, childHeight);
-            }
 
-            if (i == length - 1) {
-                width = Math.max(lineWidth, width);
-                height += lineHeight;
+                if (i == length - 1) {
+                    width = Math.max(lineWidth, width);
+                    height += lineHeight;
+                }
+            } else if (lp.positionMode == HNDivLayoutParams.POSITION_FLOAT_LEFT) {
+                mFloatLeftViews.add(child);
+            } else if (lp.positionMode == HNDivLayoutParams.POSITION_FLOAT_RIGHT) {
+                mFloatRightViews.add(child);
             }
         }
 
@@ -106,6 +116,15 @@ public class HNDiv extends ViewGroup implements IBackgroundView {
 
         setMeasuredDimension(widthMode == MeasureSpec.EXACTLY ? msWidth : width, heightMode ==
                 MeasureSpec.EXACTLY ? msHeight : height);
+    }
+
+    @Override
+    public void addView(View child, LayoutParams params) {
+        if (!(params instanceof HNDivLayoutParams)) {
+            super.addView(child, new HNDivLayoutParams(params));
+        } else {
+            super.addView(child, params);
+        }
     }
 
     @Override
@@ -129,28 +148,33 @@ public class HNDiv extends ViewGroup implements IBackgroundView {
         // 遍历所有的孩子
         for (int i = 0; i < cCount; i++) {
             View child = getChildAt(i);
-            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            HNDivLayoutParams lp = (HNDivLayoutParams) child.getLayoutParams();
             int childWidth = child.getMeasuredWidth();
             int childHeight = child.getMeasuredHeight();
 
-            // 如果已经需要换行
-            if (childWidth + lp.leftMargin + lp.rightMargin + lineWidth + paddingLeft +
-                    paddingRight > width) {
-                // 记录这一行所有的View以及最大高度
-                mLineLength.add(lineHeight);
-                // 将当前行的childView保存，然后开启新的ArrayList保存下一行的childView
-                mAllViews.add(lineViews);
-                lineWidth = childWidth;// 重置行宽
-                lineViews = new ArrayList<>();
-                lineViews.add(child);
-                lineHeight = childHeight;
-            } else {
-                /**
-                 * 如果不需要换行，则累加
-                 */
-                lineWidth += childWidth + lp.leftMargin + lp.rightMargin;
-                lineHeight = Math.max(lineHeight, childHeight + lp.topMargin + lp.bottomMargin);
-                lineViews.add(child);
+            if (lp.positionMode == HNDivLayoutParams.POSITION_STATIC || lp.positionMode ==
+                    HNDivLayoutParams.POSITION_RELATIVE) {
+                // 如果已经需要换行
+                if (childWidth + lp.leftMargin + lp.rightMargin + lineWidth + paddingLeft +
+                        paddingRight > width) {
+                    // 记录这一行所有的View以及最大高度
+                    mLineLength.add(lineHeight);
+                    // 将当前行的childView保存，然后开启新的ArrayList保存下一行的childView
+                    mAllViews.add(lineViews);
+                    lineWidth = childWidth;// 重置行宽
+                    lineViews = new ArrayList<>();
+                    lineViews.add(child);
+                    lineHeight = childHeight;
+                } else {
+                    /**
+                     * 如果不需要换行，则累加
+                     */
+                    lineWidth += childWidth + lp.leftMargin + lp.rightMargin;
+                    lineHeight = Math.max(lineHeight, childHeight + lp.topMargin + lp.bottomMargin);
+                    lineViews.add(child);
+                }
+            } else if (lp.positionMode == HNDivLayoutParams.POSITION_ABSOLUTE) {
+                child.layout(lp.left, lp.top, lp.right, lp.bottom);
             }
         }
         // 记录最后一行
@@ -173,7 +197,7 @@ public class HNDiv extends ViewGroup implements IBackgroundView {
                 if (child.getVisibility() == View.GONE) {
                     continue;
                 }
-                MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+                HNDivLayoutParams lp = (HNDivLayoutParams) child.getLayoutParams();
 
                 //计算childView的left,top,right,bottom
                 int lc = left + lp.leftMargin;
@@ -181,12 +205,76 @@ public class HNDiv extends ViewGroup implements IBackgroundView {
                 int rc = lc + child.getMeasuredWidth();
                 int bc = tc + child.getMeasuredHeight();
 
-                child.layout(lc, tc, rc, bc);
+                if (lp.positionMode == HNDivLayoutParams.POSITION_STATIC) {
+                    child.layout(lc, tc, rc, bc);
+                } else {
+                    child.layout(lc + lp.left, tc + lp.top, rc, bc);
+                }
 
                 left += child.getMeasuredWidth() + lp.rightMargin + lp.leftMargin;
             }
             left = paddingLeft;
             top += lineHeight;
+        }
+
+        top = paddingTop;
+        left = paddingLeft;
+        int lastLineHeight = 0;
+
+        for (View v : mFloatLeftViews) {
+            if (v.getVisibility() == View.GONE) {
+                continue;
+            }
+
+            HNDivLayoutParams lp = (HNDivLayoutParams) v.getLayoutParams();
+            if (left + v.getMeasuredWidth() + lp.rightMargin + lp.leftMargin > getMeasuredWidth()) {
+                left = 0;
+                top += lastLineHeight;
+                lastLineHeight = 0;
+            }
+
+            int lc = left + lp.leftMargin;
+            int tc = top + lp.topMargin;
+            int rc = lc + v.getMeasuredWidth();
+            int bc = tc + v.getMeasuredHeight();
+            Log.d("HSL", "layout " + v.getClass().getSimpleName() + ", " + String.format("%d, %d," +
+                    "" + " %d, %d", lc, tc, rc, bc));
+            v.layout(lc, tc, rc, bc);
+
+            lastLineHeight = Math.max(lastLineHeight, v.getHeight() + lp.topMargin + lp
+                    .bottomMargin);
+
+            left += v.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+        }
+
+        top = paddingTop;
+        int right = paddingRight;
+        lastLineHeight = 0;
+
+        for (View v : mFloatRightViews) {
+            if (v.getVisibility() == View.GONE) {
+                continue;
+            }
+
+            HNDivLayoutParams lp = (HNDivLayoutParams) v.getLayoutParams();
+            if (right + v.getMeasuredWidth() + lp.rightMargin + lp.leftMargin > getMeasuredWidth
+                    ()) {
+                right = 0;
+                top += lastLineHeight;
+                lastLineHeight = 0;
+            }
+
+            int lc = right + lp.rightMargin + v.getMeasuredWidth();
+            int tc = top + lp.topMargin;
+            int rc = right + lp.rightMargin;
+            int bc = tc + v.getMeasuredHeight();
+
+            v.layout(lc, tc, rc, bc);
+
+            lastLineHeight = Math.max(lastLineHeight, v.getHeight() + lp.topMargin + lp
+                    .bottomMargin);
+
+            right += v.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
         }
     }
 
@@ -200,7 +288,7 @@ public class HNDiv extends ViewGroup implements IBackgroundView {
 
     @Override
     protected ViewGroup.LayoutParams generateDefaultLayoutParams() {
-        return new MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams
+        return new HNDivLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams
                 .WRAP_CONTENT);
     }
 
@@ -235,5 +323,49 @@ public class HNDiv extends ViewGroup implements IBackgroundView {
         return new MarginLayoutParams(getContext(), attrs);
     }
 
+    public static class HNDivLayoutParams extends MarginLayoutParams {
 
+        @IntDef({POSITION_ABSOLUTE, POSITION_RELATIVE, POSITION_STATIC, POSITION_FLOAT_LEFT,
+                POSITION_FLOAT_RIGHT})
+        @Retention(RetentionPolicy.SOURCE)
+        public @interface HNDivPosition {
+
+        }
+
+        public static final int POSITION_STATIC = 0x01;
+        public static final int POSITION_RELATIVE = 0x02;
+        public static final int POSITION_ABSOLUTE = 0x03;
+        public static final int POSITION_FLOAT_LEFT = 0x04;
+        public static final int POSITION_FLOAT_RIGHT = 0x05;
+
+        public int left, top, right, bottom;
+
+        @HNDivPosition
+        public int positionMode;
+
+        public HNDivLayoutParams(int width, int height) {
+            super(width, height);
+            this.positionMode = POSITION_STATIC;
+        }
+
+        public HNDivLayoutParams(MarginLayoutParams source) {
+            super(source);
+            this.positionMode = POSITION_STATIC;
+        }
+
+        public HNDivLayoutParams(LayoutParams source) {
+            super(source);
+            this.positionMode = POSITION_STATIC;
+        }
+
+        public HNDivLayoutParams(HNDivLayoutParams source) {
+            super(source);
+
+            this.left = source.left;
+            this.top = source.top;
+            this.right = source.right;
+            this.bottom = source.bottom;
+            this.positionMode = source.positionMode;
+        }
+    }
 }
